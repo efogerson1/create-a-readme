6931372857bdfa22acbfe8bf80c2555533832c9f
efogerson1 <efogerson2@gmail.com>
2023-07-02 12:11:44 -0700

    Re-created repo, accidentally moved too many things

---
+++ generateMarkdown.js
@@ -0,0 +1,124 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+const packageJson = require('./package.json');
+
+function getLicenseBadge(packageJson) {
+  const license = packageJson.license;
+  let badge;
+
+  switch (license) {
+      case "MIT":
+          badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+          break;
+      case "Apache-2.0":
+          badge = "[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+          break;
+      case "ISC":
+          badge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+          break;
+      // Add more cases for other licenses if needed
+      default:
+          badge = "";
+          break;
+  }
+
+  return badge;
+}
+
+const licenseBadge = getLicenseBadge(packageJson);
+console.log(licenseBadge);
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+function getLicenseLink(packageJson) {
+  const license = packageJson.license;
+  let link;
+
+  switch (license) {
+      case "MIT":
+          link = "https://opensource.org/licenses/MIT";
+          break;
+      case "Apache-2.0":
+          link = "https://opensource.org/licenses/Apache-2.0";
+          break;
+      case "ISC":
+          link = "https://opensource.org/licenses/ISC";
+          break;
+      // Add more cases for other licenses if needed
+      default:
+          link = "";
+          break;
+  }
+
+  return link;
+}
+
+const licenseLink = getLicenseLink(packageJson);
+console.log(licenseLink);
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+function generateLicenseSection(packageJson) {
+  const license = packageJson.license;
+  let licenseSection;
+
+  switch (license) {
+      case "MIT":
+          licenseSection = `
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+`;
+          break;
+      case "Apache-2.0":
+          licenseSection = `
+## License
+
+This project is licensed under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0).
+`;
+          break;
+      case "ISC":
+          licenseSection = `
+## License
+
+This project is licensed under the [ISC License](https://opensource.org/licenses/ISC).
+`;
+          break;
+      // Add more cases for other licenses if needed
+      default:
+          licenseSection = "";
+          break;
+  }
+
+  return licenseSection;
+}
+
+const licenseSection = generateLicenseSection(packageJson);
+console.log(licenseSection);
+
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(packageJson) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = {
+  generateMarkdown,
+  getLicenseBadge,
+  getLicenseLink,
+  generateLicenseSection,
+};
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,92 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const generateHTML = ({ name, location, hobby, food, github, linkedin }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="p-5 mb-4 header bg-light">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${name}</h1>
+      <p class="lead">I am from ${location}.</p>
+      <p class="lead">My favorite hobby is ${hobby}.</p>
+      <p class="lead">My favorite food is ${food}.</p>
+      <h3>Cantact Info <span class="badge bg-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${github}</li>
+        <li class="list-group-item">LinkedIn: ${linkedin}</li>
+        
+      </ul>
+    </div>
+  </header>
+</body>
+</html>`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
+
+
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to write README file
+
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
